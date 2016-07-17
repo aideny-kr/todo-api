6fca67ae0e13c67eda3deeca53063ba4627b52bf
Chan Yi <huichanyi@gmail.com>
2016-07-17 07:43:18 -0400

    Added Underscore and refactored code

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -21,32 +23,35 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = +req.params.id;
-  var matchedTodo;
-  todos.forEach(function (todo) {
-    if (todoId === todo.id) {
-      matchedTodo = todo;
-    }
-  });
-
-  if(matchedTodo) res.json(matchedTodo);
+  var matchedTodo = _.findWhere(todos, {id : todoId});
+  if(matchedTodo) {
+    res.json(matchedTodo);
+  }
   else res.status(404).send();
 
-  res.send('Asking for todo with id of ' + req.params.id);
 });
 
 // POST /todos
 app.post('/todos', function(req, res) {
-  var body = req.body;
-  if(body) {
-    // set id to todoNextId and increment todoNextId
-    body.id = todoNextId++;
-    // push to todos array
-    todos.push(body);
+
+  var body = _.pick(req.body, 'description', 'completed')
+
+  if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+    // sending response "bad request"
+    return res.status(400).send();
   }
+
+  body.description = body.description.trim()
+
+    // set id to todoNextId and increment todoNextId
+  body.id = todoNextId++;
+    // push to todos array
+  todos.push(body);
+
 
   res.json(body);
 });
 
 app.listen(PORT, function() {
   console.log('Express listening on PORT ' + PORT);
-})
+});
