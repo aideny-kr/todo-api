7e31f70441413745ef532941a27366cd2424bfca
Chan Yi <huichanyi@gmail.com>
2016-07-21 16:48:06 -0400

    Add ?completed query

--- server.js
+++ server.js
@@ -17,7 +17,16 @@ app.get('/', function (req, res) {
 
 // GET /todos
 app.get('/todos', function (req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    filteredTodos = _.where(filteredTodos, {completed: true});
+  } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {completed: false});
+  }
+
+  res.json(filteredTodos);
 });
 
 // GET /todos/:id
@@ -93,7 +102,7 @@ app.put('/todos/:id', function (req, res) {
   } else if (body.hasOwnProperty('description')) {
     return res.status(400).send();
   }
-
+  // extend alters first argument(object) according to 2nd argument
   _.extend(matchedTodo, validAttributes);
 
   res.json(matchedTodo);
