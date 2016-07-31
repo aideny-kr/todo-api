8ce997641025b4ed7cc9433a22c133b345df1b8f
Chan Yi <huichanyi@gmail.com>
2016-07-31 08:10:16 -0400

    Convert Get /todos to sequelize

--- server.js
+++ server.js
@@ -21,23 +21,47 @@ app.get('/todos', function (req, res) {
 
   // query string ?completed=true
   var queryParams = req.query;
-  var filteredTodos = todos;
+  var where = {};
 
-  // ?complete=true/false
   if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-    filteredTodos = _.where(filteredTodos, {completed: true});
+    where.completed = true;
   } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-    filteredTodos = _.where(filteredTodos, {completed: false});
+    where.completed = false;
   }
 
-  // ?q=keyword
-  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-    filteredTodos = _.filter(filteredTodos, function(todo) {
-      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-    });
+  if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    where.description = {$like : '%' + queryParams.q + '%'};
   }
 
-  res.json(filteredTodos);
+  db.todo.findAll({
+    where: where
+  }).then(function(todos) {
+    if(todos) {
+      res.json(todos);
+    } else {
+      res.status(404).send();
+    }
+  }, function(e) {
+    res.status(500).send();
+  });
+
+  // var filteredTodos = todos;
+  //
+  // // ?complete=true/false
+  // if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+  //   filteredTodos = _.where(filteredTodos, {completed: true});
+  // } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+  //   filteredTodos = _.where(filteredTodos, {completed: false});
+  // }
+  //
+  // // ?q=keyword
+  // if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+  //   filteredTodos = _.filter(filteredTodos, function(todo) {
+  //     return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+  //   });
+  // }
+  //
+  // res.json(filteredTodos);
 });
 
 // GET /todos/:id
