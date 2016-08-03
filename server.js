058fcc84abf0574385d3f540ee6cf910f6339607
Chan Yi <huichanyi@gmail.com>
2016-08-02 22:46:16 -0400

    Convert PUT todos to sequelize

--- server.js
+++ server.js
@@ -150,34 +150,31 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function (req, res) {
   var todoId = +req.params.id;
-  var matchedTodo = _.findWhere(todos, {id: todoId});
   var body = _.pick(req.body, 'description', 'completed');
-  var validAttributes = {};
-
-  // 404 not found
-  if(!matchedTodo) {
-    return res.status(404).send();
-  }
+  var attributes = {};
 
   // validates 'completed'
-  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-    validAttributes.completed = body.completed;
-  } else if (body.hasOwnProperty('completed')) {
-    // 400 bad syntax
-    return res.status(400).send();
+  if (body.hasOwnProperty('completed')) {
+    attributes.completed = body.completed;
   }
-
   // validates 'description'
-  if (body.hasOwnProperty('description') && _.isString(body.description)
-  && body.description.trim().length > 0) {
-    validAttributes.description = body.description;
-  } else if (body.hasOwnProperty('description')) {
-    return res.status(400).send();
+  if (body.hasOwnProperty('description')) {
+    attributes.description = body.description;
   }
-  // extend alters first argument(object) according to 2nd argument
-  _.extend(matchedTodo, validAttributes);
 
-  res.json(matchedTodo);
+  db.todo.findById(todoId).then(function (todo){
+    if(todo) {
+      todo.update(attributes).then(function (todo) {
+        res.json(todo.toJSON());
+      }, function(e) {
+        res.status(400).json(e);
+      });
+    } else {
+      res.status(404).send();
+    }
+  }, function(e) {
+    res.status(500).send();
+  });
 
 });
 
