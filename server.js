37fb99d18d3f0b6a10750c7dda629c056fa634ab
Chan Yi <huichanyi@gmail.com>
2016-08-11 20:39:50 -0400

    Added authentication classMethod

--- server.js
+++ server.js
@@ -196,26 +196,13 @@ app.post('/users', function(req, res){
 // POST /users/login
 app.post('/users/login', function(req, res) {
   var body = _.pick(req.body, 'email', 'password');
-  if(typeof body.email !== 'string' || typeof body.password !== 'string') {
-    return res.status(404).send();
-  }
 
-  db.user.findOne({
-    where: {
-      email: body.email
-    }
-  }).then(function(user) {
-    if(!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-      return res.status(401).send();
-    }
-
+  db.user.authenticate(body).then(function(user) {
     res.json(user.toPublicJSON());
   }, function(e) {
-    res.status(500).send();
+    res.status(401).send();
   });
-
 });
-
 
 db.sequelize.sync().then(function(){
 
